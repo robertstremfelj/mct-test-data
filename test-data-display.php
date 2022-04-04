3e52626cdaedf38a5a66cbcda5f3d755d5125413
robertstremfelj <robert.stremfelj@agencija-lars.si>
2022-04-04 10:59:05 +0200

    Added files

---
+++ test-data-display.php
@@ -0,0 +1,50 @@
+<?php
+
+if (!defined('ABSPATH')) exit;
+
+class testDataDisplay
+{
+    
+    public static function init() {
+        $class = __CLASS__;
+        new $class;
+    }
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    public function __construct() {
+        add_action('rest_api_init', [$this, 'registerRoutes']);
+    }
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    public function registerRoutes() {
+		register_rest_route(
+			'test/v1',
+			'/test-data',
+			[
+				'methods'             => 'GET',
+				'callback'            => [$this, 'test_data'],
+				'permission_callback' => [$this, 'auth']
+			]
+		);
+	}
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+	public function auth() {
+		return isset( $_GET['auth'] ) && $_GET['auth'] == '1576f59420f3dd49d858841d2657ae30';
+	}
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    public function test_data() {
+
+        header('Content-Type: text/html; charset=UTF-8');
+        echo '<div style="padding:10px;border:1px solid #cccccc;background:#eeeeee;">Testiramo kako deluje plugin preko WP Pusherja!</div>';
+
+    }
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    
+}
---
+++ test-data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Test data plugin
+ * Plugin URI: https://www.popolnapostava.com/
+ * Description: Plugin, ki ustvari test data.
+ * Version: 1.0.0.
+ * Author: Popolna Postava
+ * Author URI: https://www.popolnapostava.com/
+ */
+
+if (!defined('ABSPATH')) exit;
+
+require_once dirname(__FILE__) . '/test-data-display.php';
+add_action('plugins_loaded', array( 'testDataDisplay', 'init'));
